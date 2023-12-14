58d972909f66062e4e95f4841e214266b6116c0a
naveen.ta <naveen.ta@CLAP992>
2023-12-14 10:42:55 +0530

    Add project files.

---
+++ Prodapt.CSharp.Day4/Program.cs
@@ -0,0 +1,13 @@
+﻿using Prodapt.CSharp.Library;
+namespace Prodapt.CSharp.Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LoginValidation obj = new LoginValidation();
+            var result = obj.Validation("admin", "admin@123");
+            Console.WriteLine(result ? "Valid User" : "Invalid User");
+        }
+    }
+}
---
+++ Prodapt.CSharp.Day5/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Prodapt.CSharp.Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var date=DateTime.Now;
+            Console.WriteLine(DateTime.Now.AddDays(10));
+            Console.WriteLine(date);
+            Console.WriteLine("Hello");
+        }
+    }
+}
